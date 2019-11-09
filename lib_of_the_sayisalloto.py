4c34d2330c7f67131eb33dd926ee108b7a411d9a
Hüseyin Emre Girgin <e.girgin37@gmail.com>
2019-11-09 03:54:00 +0300

    test_array_creator_and_saver() added

--- lib_of_the_sayisalloto.py
+++ lib_of_the_sayisalloto.py
@@ -154,3 +154,96 @@ def seperated_array_for_train():
 
     return x_array, y_array
 
+def test_array_creator_and_saver():
+
+    import numpy as np
+    import random as rand
+
+    """---------------------------------------------------------------"""
+    """               We are loading our lotodataset.npz              """
+    """---------------------------------------------------------------"""
+
+    (d, m, y) = seperate_date()
+    w = week_get()
+    (tablo, length) = import_table()
+
+    ball1_test = []
+    ball2_test = []
+    ball3_test = []
+    ball4_test = []
+    ball5_test = []
+    ball6_test = []
+
+    ball1 = ball_1_get(tablo, length)
+    ball2 = ball_2_get(tablo, length)
+    ball3 = ball_3_get(tablo, length)
+    ball4 = ball_4_get(tablo, length)
+    ball5 = ball_5_get(tablo, length)
+    ball6 = ball_6_get(tablo, length)
+
+    """---------------------------------------------------------------"""
+    """              Create Test File Randomly                        """
+    """---------------------------------------------------------------"""
+
+    test_file_size = 100
+    lenn = len(d[0])
+
+    for i in range(test_file_size):
+        lenn = len(d)
+        random = rand.randint(0, lenn)
+        if i == 0:
+            x_test = np.array([[[w[0][random], d[0][random]], [m[0][random], y[0][random]]]], dtype=float)
+            w = np.delete(w[0], random)
+            d = np.delete(d[0], random)
+            m = np.delete(m[0], random)
+            y = np.delete(y[0], random)
+            ball1_test.append(ball1[random])
+            ball2_test.append(ball2[random])
+            ball3_test.append(ball3[random])
+            ball4_test.append(ball4[random])
+            ball5_test.append(ball5[random])
+            ball6_test.append(ball6[random])
+            del ball1[random], ball2[random], ball3[random], ball4[random], ball5[random], ball6[random]
+        else:
+            random_pick_ba = np.array([[[w[random], d[random]], [m[random], y[random]]]], dtype=float)
+            x_test = np.vstack((x_test, random_pick_ba))
+            w = np.delete(w, random)
+            d = np.delete(d, random)
+            m = np.delete(m, random)
+            y = np.delete(y, random)
+            ball1_test.append(ball1[random])
+            ball2_test.append(ball2[random])
+            ball3_test.append(ball3[random])
+            ball4_test.append(ball4[random])
+            ball5_test.append(ball5[random])
+            ball6_test.append(ball6[random])
+            del ball1[random], ball2[random], ball3[random], ball4[random], ball5[random], ball6[random]
+
+    for i in range(len(d)):
+        if i == 0:
+            x_train = np.array([[[w[i], d[i]], [m[i], y[i]]]], dtype=float)
+        else:
+            new_block = np.array([[[w[i], d[i]], [m[i], y[i]]]], dtype=float)
+            x_train = np.vstack((x_train, new_block))
+
+    ball1_train = np.transpose(np.array([ball1], dtype=float))
+    ball2_train = np.transpose(np.array([ball2], dtype=float))
+    ball3_train = np.transpose(np.array([ball3], dtype=float))
+    ball4_train = np.transpose(np.array([ball4], dtype=float))
+    ball5_train = np.transpose(np.array([ball5], dtype=float))
+    ball6_train = np.transpose(np.array([ball6], dtype=float))
+
+    ball1_test_array = np.transpose(np.array([ball1_test], dtype=float))
+    ball2_test_array = np.transpose(np.array([ball2_test], dtype=float))
+    ball3_test_array = np.transpose(np.array([ball3_test], dtype=float))
+    ball4_test_array = np.transpose(np.array([ball4_test], dtype=float))
+    ball5_test_array = np.transpose(np.array([ball5_test], dtype=float))
+    ball6_test_array = np.transpose(np.array([ball6_test], dtype=float))
+
+    np.savez(
+        'lotodatasetv3.npz', InputsTrain=x_train, InputsTest=x_test,
+        y1_train=ball1_train, y2_train=ball2_train, y3_train=ball3_train,
+        y4_train=ball4_train, y5_train=ball5_train, y6_train=ball6_train,
+        y1_test=ball1_test_array, y2_test=ball2_test_array, y3_test=ball3_test_array,
+        y4_test=ball4_test_array, y5_test=ball5_test_array, y6_test=ball6_test_array
+    )
